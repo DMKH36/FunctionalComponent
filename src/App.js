6791b78d84816a520fe0218732ceb59e37affbb2
Muhammad Abdul Majid <muhammadabdulmajid36@Gmail.com>
2021-09-12 02:58:15 +0700

    Done Functional Komponen 1 - tabel data diri

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pt-4 pb-4 text-3xl text-center bg-green-300">
+      <h1 className="font-sans font-extrabold">Tugas Modul 2</h1>
+      <h2 className="pt-2 font-mono font-normal">Kelompok 44</h2>
     </div>
-  );
+  )
 }
-
 export default App;
